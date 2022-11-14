28abd01680174781c5473eecc09daaba4da2abe7
vitorgabrielgit <vitorgabrieldeoliveiragit@gmail.com>
2022-11-14 11:05:41 -0300

    INSERT ok

--- index.js
+++ index.js
@@ -3,11 +3,6 @@
     const db = require("./db");
     console.log('Começou!');
     
-    console.log('INSERT INTO users;');
-    const result = await db.insertCustomer({nome: "Zé", idade: 18});
+    const result = await db.insertCustomer({nome: "Vitor Gabriel De Oliveira", idade: 15});
     console.log(result);
- 
-    console.log('SELECT * FROM users;');
-    const clientes = await db.selectCustomers();
-    console.log(clientes);
 })();
